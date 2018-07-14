1623ab74eecfe06e9590c2477fff2a346eecfad1
mergitto <merugitosu@gmail.ccom>
2018-07-14 21:16:14 +0900

    分散処理できるようにした

--- not_same_text.py
+++ not_same_text.py
@@ -54,13 +54,29 @@ def recovery_list(match_list, cleansing_text_list, origin_text_all_list):
         cleansing_text_list.append(origin_text_all_list[recovery_number])
     return cleansing_text_list
 
+def distributed_processing(text_all_list, process_number):
+    SPLIT_NUMBER = process_number
+    START_INDEX = 0
+    END_INDEX = process_number
+    match_all_list = []
+    cleansing_text_all_list = []
+    while True:
+        if len(text_all_list[START_INDEX:END_INDEX]) == 0:
+            break
+        print(END_INDEX, '/', len(text_all_list))
+        match_list, cleansing_text_list = delete_if_same(text_all_list[START_INDEX:END_INDEX])
+        match_all_list += match_list
+        cleansing_text_all_list += cleansing_text_list
+        START_INDEX += SPLIT_NUMBER
+        END_INDEX += SPLIT_NUMBER
+    return match_all_list, cleansing_text_all_list
 
 if __name__ == '__main__':
     calculation = calc.Calc()
     text_all_list = create_text_list(sys.argv[1])
     origin_text_all_list = text_all_list[:]
 
-    match_list, cleansing_text_list = delete_if_same(text_all_list)
+    match_list, cleansing_text_list = distributed_processing(text_all_list, 10000)
     recovery_text_list = recovery_list(match_list, cleansing_text_list, origin_text_all_list)
 
     save_text(recovery_text_list)
